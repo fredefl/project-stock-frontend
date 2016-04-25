0e8691ef7613828321f3896e44afc6df1cb80e1a
Bo Hans Thomsen <bht@it-minds.dk>
2016-04-25 10:54:10 +0200

    Added correct structure

--- src/browser/partner/Page.react.js
+++ src/browser/partner/Page.react.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 /* Material UI */
-import {TextField, List, ListItem} from 'material-ui';
+import {TextField, List, ListItem, RaisedButton, Paper} from 'material-ui';
 import Formsy from 'formsy-react';
 import {FormsyText} from 'formsy-material-ui';
 
@@ -17,7 +17,16 @@ class Page extends Component {
     this.state = {
       canSubmit: false
     }
+
+    this.enableButton = this.enableButton.bind(this);
+    this.disableButton = this.disableButton.bind(this);
+    this.submit = this.submit.bind(this);
   }
+
+  errorMessages = {
+    titleError: "The title must be words",
+    descriptionError: "The description must be words"
+  };
 
   enableButton () {
     this.setState({
@@ -35,20 +44,47 @@ class Page extends Component {
 
   }
 
-  errorMessages : {
-    title: "The title must be words",
-    description: "The description must be words"
-  }
-
   static propTypes = {
     msg: PropTypes.object.isRequired
   };
 
   render() {
     const { msg } = this.props;
+    let {titleError, descriptionError} = this.errorMessages;
 
     return (
-      <div></div>
+      <div className="partner-page">
+        <Paper style={{marginTop: "30px", padding: "30px"}}>
+          <h1>Create Project</h1>
+
+          <Formsy.Form onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+            <FormsyText
+             name="title"
+             validations="isWords"
+             validationError={titleError}
+             required
+             hintText="Title text"
+             floatingLabelText="Title"
+             className="input-div" />
+            <FormsyText
+             name="description"
+             validations="isWords"
+             validationError={descriptionError}
+             hintText="Description text"
+             floatingLabelText="Description"
+             required
+             className="input-div" />
+            <FormsyText
+             name="author"
+             validations="isWords"
+             hintText="Author text"
+             floatingLabelText="Author"
+             required
+             className="input-div" />
+            <RaisedButton label="Save" primary={true} disabled={!this.state.canSubmit} className="button-div" />
+          </Formsy.Form>
+        </Paper>
+      </div>
     );
   }
 
