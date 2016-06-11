32c95101086eb4aabe7d58113308eaeaa5f0f16b
Frederik Lassen <frederiklassen@gmail.com>
2016-06-11 16:34:56 +0200

    Adds descriptions from API

--- src/browser/project/ProjectCard.react.js
+++ src/browser/project/ProjectCard.react.js
@@ -43,10 +43,7 @@ class ProjectCard extends Component {
           subtitle={project.subtitle}
         />
         <CardText className="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          {project.description}
         </CardText>
         <CardActions className="actions">
         </CardActions>
--- src/browser/projects/ProjectCard.react.js
+++ src/browser/projects/ProjectCard.react.js
@@ -51,10 +51,7 @@ class ProjectCard extends Component {
           subtitle={project.subtitle}
         />
         <CardText className="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          {project.short_description}
         </CardText>
         <CardActions className="actions">
           <FlatButton label="View" containerElement={<Link to={`/project/${project.id}`} />} />
